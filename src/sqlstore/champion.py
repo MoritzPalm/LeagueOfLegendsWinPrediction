27948596f6a21870b5df1a633b7a812b154f09cf
Moritz Palm <moritz.palm@web.de>
2023-08-28 06:17:51 +0200

    fixed issue with champion table

--- src/sqlstore/champion.py
+++ src/sqlstore/champion.py
@@ -4,7 +4,7 @@ from src.sqlstore.db import Base
 
 
 class SQLChampion(Base):
-    tablename = "champion"
+    __tablename__ = "champion"
 
     championId = Column(Integer, primary_key=True)
     championName = Column(String(100))
@@ -13,9 +13,10 @@ class SQLChampion(Base):
     skillLevel = Column(Integer, CheckConstraint('skillLevel>=1 AND skillLevel<=5'), nullable=True)
     patchWinRate = Column(Float, nullable=True)  # Represented as a percent
     patchPlayRate = Column(Float, nullable=True)  # Represented as a percent
-    patchNumber = Column(String(20), nullable=True)  # Representing which game patch
-    role = Column(String(50), nullable=True)  # Top, Mid...
-    # Maybe counters, abilities, Tier, maybe range, skill-shot-based, or not, cc-level.., trends in winrates, role flexibility, new skin released (higher playrate)
+    patchNumber = Column(String(20), nullable=True)  # Representing which game patch # TODO: should this be part of the primary key?
+    primaryRole = Column(String(50), nullable=True)  # Top, Mid...
+    # Maybe counters, abilities, Tier, maybe range, skill-shot-based, or not, cc-level.., trends in winrates,
+    # role flexibility, new skin released (higher playrate)
 
     def init(self, championId, championName, championTitle, championLore=None, skillLevel=None,
                  patchWinRate=None, patchPlayRate=None, patchNumber=None, role=None):
@@ -27,7 +28,7 @@ class SQLChampion(Base):
         self.patchWinRate = patchWinRate
         self.patchPlayRate = patchPlayRate
         self.patchNumber = patchNumber
-        self.role = role
+        self.primaryRole = role
 
     def repr(self):
         return f"<Champion {self.championName} - {self.championTitle}>"
