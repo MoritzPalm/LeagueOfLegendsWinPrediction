914410857631fc6e05bb3bad4ec69a5613e56ae1
Moritz Palm <moritz.palm@web.de>
2023-09-14 14:57:11 +0200

    reordered appending of participant object to match object to fix error where matchId would be empty

--- src/parsers/participant.py
+++ src/parsers/participant.py
@@ -12,28 +12,29 @@ def parse_participant_data(session: sqlalchemy.orm.Session, match: SQLMatch, par
     :param participant: list of dicts containing participant stats
     :return: None
     """
-    participant_obj = SQLParticipant(puuid=participant['puuid'], participantId=participant['participantId'])
-    session.add(participant_obj)
-    participantStats_obj = SQLParticipantStats(**participant)
-    match.participant.append(participant_obj)
-    participant_obj.stats.append(participantStats_obj)
-    session.add(participantStats_obj)
-    statPerks = participant['perks']['statPerks']
-    participantPerk_obj = SQLStatPerk(participant['puuid'], statPerks['defense'], statPerks['flex'],
-                                      statPerks['offense'])
-    participant_obj.statPerks.append(participantPerk_obj)
-    session.add(participantPerk_obj)
-    styles = participant['perks']['styles']
-    for style in styles:
-        participantStyle_obj = SQLStyle(style['description'], style['style'])
-        participant_obj.styles.append(participantStyle_obj)
-        for selection in style['selections']:
-            participantStyleSelection_obj = SQLStyleSelection(selection['perk'], selection['var1'],
-                                                              selection['var2'],
-                                                              selection['var3'])
-            participantStyle_obj.selection.append(participantStyleSelection_obj)
-            session.add(participantStyleSelection_obj)
-    participant['challenges']['Assist12StreakCount'] = participant['challenges']['12AssistStreakCount']  # rename
-    participantChallenges_obj = SQLChallenges(**participant['challenges'])
-    participant_obj.challenges.append(participantChallenges_obj)
-    session.add(participantChallenges_obj)
+    with session.no_autoflush:
+        participant_obj = SQLParticipant(puuid=participant['puuid'], participantId=participant['participantId'])
+        match.participant.append(participant_obj)
+        session.add(participant_obj)
+        participantStats_obj = SQLParticipantStats(**participant)
+        participant_obj.stats.append(participantStats_obj)
+        session.add(participantStats_obj)
+        statPerks = participant['perks']['statPerks']
+        participantPerk_obj = SQLStatPerk(participant['puuid'], statPerks['defense'], statPerks['flex'],
+                                          statPerks['offense'])
+        participant_obj.statPerks.append(participantPerk_obj)
+        session.add(participantPerk_obj)
+        styles = participant['perks']['styles']
+        for style in styles:
+            participantStyle_obj = SQLStyle(style['description'], style['style'])
+            participant_obj.styles.append(participantStyle_obj)
+            for selection in style['selections']:
+                participantStyleSelection_obj = SQLStyleSelection(selection['perk'], selection['var1'],
+                                                                  selection['var2'],
+                                                                  selection['var3'])
+                participantStyle_obj.selection.append(participantStyleSelection_obj)
+                session.add(participantStyleSelection_obj)
+        participant['challenges']['Assist12StreakCount'] = participant['challenges']['12AssistStreakCount']  # rename
+        participantChallenges_obj = SQLChallenges(**participant['challenges'])
+        participant_obj.challenges.append(participantChallenges_obj)
+        session.add(participantChallenges_obj)
