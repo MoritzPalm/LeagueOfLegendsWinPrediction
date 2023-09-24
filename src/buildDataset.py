6ee6a37d85718b3d74f4ceb1e3fc845d23a50619
Moritz Palm <moritz.palm@web.de>
2023-09-24 18:20:29 +0200

    removed participant stats as they are available only after the match and added concat of dataframe rows (building dataframe of matches)

--- src/buildDataset.py
+++ src/buildDataset.py
@@ -11,14 +11,14 @@ import pandas as pd
 import numpy as np
 
 
-def build_dataset(size: int):
+def build_static_dataset(size: int) -> pd.DataFrame:
     """
-
-    :param session:
-    :param size: number of matches
-    :return:
+    builds dataset with all static information (info available prior to match start) from database
+    :param size: number of matches in dataset
+    :return: Dataframe with large number of columns (features)
     """
     with get_session() as session:
+        data = pd.DataFrame()
         matches = session.query(SQLMatch).order_by(func.random()).limit(size).all()
         for match in matches:
             # wrapping the dict in a list to prevent missing index issues, this is hacky and may have unforeseen issues
@@ -26,20 +26,20 @@ def build_dataset(size: int):
             participants = session.query(SQLParticipant).filter(SQLParticipant.matchId == match.matchId).all()
             assert len(participants) == 10
             for i, participant in enumerate(participants):
-                stat: SQLParticipantStats = session.query(SQLParticipantStats).filter(
-                    SQLParticipantStats.participantId == participant.id).one()
-                df_participant = pd.DataFrame([stat.__dict__])
-                # renames all columns to have a participant and the number in front of the attribute
-                df_participant.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
                 summoner = session.query(SQLSummoner).filter(SQLSummoner.puuid == participant.puuid).one()
                 df_summoner = pd.DataFrame([summoner.__dict__])
+                # renames all columns to have a participant and the number in front of the attribute
                 df_summoner.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                #mastery = session.query(SQLChampionMastery).filter(SQLChampionMastery.puuid == participant.puuid,
-                                                                #SQLChampionMastery.championId == stat.championId).one()
-                #df_mastery = pd.DataFrame([mastery.__dict__])
-                #df_mastery.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                #appending all dataframes to df_match
-                df_match = pd.concat([df_match, df_participant, df_summoner], axis=1, copy=False)
+                summonerLeague = session.query(SQLSummonerLeague).filter(
+                    SQLSummonerLeague.puuid == participant.puuid).one()
+                df_summonerLeague = pd.DataFrame([summonerLeague.__dict__])
+                df_summonerLeague.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
+                # mastery = session.query(SQLChampionMastery).filter(SQLChampionMastery.puuid == participant.puuid,
+                # SQLChampionMastery.championId == stat.championId).one()
+                # df_mastery = pd.DataFrame([mastery.__dict__])
+                # df_mastery.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
+                # appending all dataframes to df_match
+                df_match = pd.concat([df_match, df_summoner, df_summonerLeague], axis=1, copy=False)
                 print(df_match.shape)
-    print("test")
-
+            data = pd.concat([data, df_match], axis=0, copy=False)
+    return data
