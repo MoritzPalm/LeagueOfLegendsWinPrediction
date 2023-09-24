a325ed61ed4438769ffbbdb67476d9f1accbe55d
Moritz Palm <moritz.palm@web.de>
2023-09-24 21:56:50 +0200

    changed selenium to scrapy

--- src/crawlers/scraping.py
+++ src/crawlers/scraping.py
@@ -10,64 +10,60 @@ import logging
 
 from src import utils
 
+from scrapy import Spider
+from scrapy.crawler import CrawlerRunner
+from twisted.internet import reactor
+from scrapy.utils.log import configure_logging
+import csv
+from io import StringIO
 
-def scrape_summonerdata(name: str, region: str) -> pd.DataFrame:
-    # URL
-    url = f"https://u.gg/lol/profile/{region}/{name}/champion-stats"
 
-    options = Options()
-    options.add_argument("--headless")
+class MySpider(Spider):
+    name = 'my_spider'
+    custom_settings = {'LOG_LEVEL': 'INFO'}
 
-    driver = webdriver.Chrome(options=options)
+    def __init__(self, summoner_name, *args, **kwargs):
+        super(MySpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f"https://u.gg/lol/profile/euw1/{summoner_name}/champion-stats"]
 
-    driver.get(url)
+    def parse(self, response):
+        data = []
+        columns = ['Rank', 'Champion', 'Win Rate', 'Wins/Loses', 'Unnamed', 'Kills', 'Deaths', 'Assists', 'LP',
+                   'Max Kills', 'Max Deaths', 'CS', 'Damage', 'Gold']
 
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.rt-tr-group")))
-
-    # Initialize list to store each row of data
-    data = []
-
-    # Get page source and create BeautifulSoup object
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    # Close the driver
-    driver.quit()
-
-    # Get all the rows using BeautifulSoup
-    rows = soup.select("div.rt-tr-group")
-
-    for i, row in enumerate(rows, 1):  # start from 1 because CSS nth-child starts from 1
         try:
-            selectors = {
-                'Champion': "div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)",
-                'WinsLoses': "div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(3)",
-                'Winrate': "div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > strong:nth-child(1)",
-                'KDA': "div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > strong:nth-child(1)",
-                'KillsDeathsAssists': "div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > span:nth-child(2)",
-                # New field
-                'LP': "div:nth-child(1) > div:nth-child(5) > span:nth-child(1)",
-                'MaxKills': "div:nth-child(1) > div:nth-child(6) > span:nth-child(1)",
-                'MaxDeaths': "div:nth-child(1) > div:nth-child(7)",
-                'CS': "div:nth-child(1) > div:nth-child(8) > span:nth-child(1)",
-                'Damage': "div:nth-child(1) > div:nth-child(9) > span:nth-child(1)",
-                'Gold': "div:nth-child(1) > div:nth-child(10) > span:nth-child(1)"
-            }
+            row = []
+            selectors = [
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > strong:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(3)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > strong:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > span:nth-child(2) > strong:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > span:nth-child(2) > strong:nth-child(3)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > span:nth-child(2) > strong:nth-child(5)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(5) > span:nth-child(1) > span:nth-child(2)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(6) > span:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(7) > span:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(8) > span:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(9) > span:nth-child(1)::text",
+                "div.rt-tr-group:nth-child(1) > div:nth-child(1) > div:nth-child(10) > span:nth-child(1)::text",
+            ]
+            for selector in selectors:
+                item = response.css(selector).get()
+                row.append(item.strip() if item else 'N/A')
+            data.append(row)
 
-            row_data = []
-            for key, selector in selectors.items():
-                element = row.select_one(selector)
-                if element:
-                    text = element.get_text(strip=True)
-                    if key in ['Damage', 'Gold']:
-                        text = text.replace(',', '')
-                else:
-                    text = 'N/A'
-                row_data.append(text)
-            data.append(row_data)
         except Exception as e:
-            logging.warning(f"Error in row {i}: {e}")
+            self.log(f"Error: {e}")
 
+
+def run_spider(summoner_name):
+    configure_logging({'LOG_LEVEL': 'INFO'})
+    runner = CrawlerRunner()
+    d = runner.crawl(MySpider, summoner_name=summoner_name)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
 
     # Convert the data to a DataFrame
     columns = ['Champion', 'WinsLoses', 'Winrate', 'KDA', 'KillsDeathsAssists', 'LP', 'MaxKills', 'MaxDeaths', 'CS',
@@ -142,4 +138,3 @@ def scrape_champion_metrics():
     df_scraped = utils.clean_champion_data(df_scraped)
     # Return the DataFrame converted to a dictionary, indexed by "Champion Name"
     return df_scraped.reset_index().to_dict('index')
-
