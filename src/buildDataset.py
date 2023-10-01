5599d0a63f60569cf3a3d4b7815f15c98894e6f0
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-10-01 14:02:03 +0200

    Updated build_static_dataset to fetch the mastery data and concatenate it into the dataframe correctly. Needs testing

--- src/buildDataset.py
+++ src/buildDataset.py
@@ -13,33 +13,52 @@ import numpy as np
 
 def build_static_dataset(size: int) -> pd.DataFrame:
     """
-    builds dataset with all static information (info available prior to match start) from database
-    :param size: number of matches in dataset
-    :return: Dataframe with large number of columns (features)
+    Builds a dataset with all static information (info available prior to match start) from the database.
+    :param size: Number of matches in the dataset
+    :return: DataFrame with a large number of columns (features)
     """
     with get_session() as session:
+        # Initialize an empty DataFrame
         data = pd.DataFrame()
+
+        # Fetch a specific number of random matches from the SQLMatch table
         matches = session.query(SQLMatch).order_by(func.random()).limit(size).all()
+
         for match in matches:
-            match: SQLMatch
-            # wrapping the dict in a list to prevent missing index issues, this is hacky and may have unforeseen issues
+            # Create a DataFrame for each match's data
             df_match = pd.DataFrame([match.get_training_data()])
+
+            # Query participants for the current match
             participants = session.query(SQLParticipant).filter(SQLParticipant.matchId == match.matchId).all()
+
+            # Ensure there are exactly 10 participants in the match
             assert len(participants) == 10
+
             for i, participant in enumerate(participants):
-                summoner: SQLSummoner = session.query(SQLSummoner).filter(SQLSummoner.puuid == participant.puuid).one()
+                # Fetch Summoner data
+                summoner = session.query(SQLSummoner).filter(SQLSummoner.puuid == participant.puuid).one()
                 df_summoner = pd.DataFrame([summoner.get_training_data()])
-                # renames all columns to have a participant and the number in front of the attribute
                 df_summoner.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                summonerLeague: SQLSummonerLeague = session.query(SQLSummonerLeague).filter(
+
+                # Fetch Summoner League data
+                summonerLeague = session.query(SQLSummonerLeague).filter(
                     SQLSummonerLeague.puuid == participant.puuid).one()
                 df_summonerLeague = pd.DataFrame([summonerLeague.get_training_data()])
                 df_summonerLeague.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                # mastery: SQLChampionMastery = session.query(SQLChampionMastery).filter(SQLChampionMastery.puuid == participant.puuid,
-                # SQLChampionMastery.championId == stat.championId).one()
-                # df_mastery = pd.DataFrame([mastery.get_training_data])
-                # df_mastery.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                # appending all dataframes to df_match
-                df_match = pd.concat([df_match, df_summoner, df_summonerLeague], axis=1, copy=False)
+
+                # Fetch Mastery data
+                champion_id = participant.championId
+                mastery = session.query(SQLChampionMastery).filter(
+                    SQLChampionMastery.puuid == participant.puuid,
+                    SQLChampionMastery.championId == champion_id).one()
+                df_mastery = pd.DataFrame([mastery.get_training_data()])
+                df_mastery.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
+
+                # Concatenate Summoner, Summoner League, and Mastery data to the match DataFrame
+                df_match = pd.concat([df_match, df_summoner, df_summonerLeague, df_mastery], axis=1, copy=False)
+
+            # Append this match's DataFrame to the overall DataFrame
             data = pd.concat([data, df_match], axis=0, copy=False)
+            print(build_static_dataset(size=2))
     return data
+
