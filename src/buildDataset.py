5e86404585dcef93b81cb08226b810d5a3aa09ce
Moritz Palm <moritz.palm@web.de>
2023-10-04 08:15:37 +0200

    fixed issues when building dataset from database

--- src/buildDataset.py
+++ src/buildDataset.py
@@ -1,4 +1,6 @@
 from typing import Tuple, Any
+import pickle
+import os
 
 import sqlalchemy.orm
 from sqlalchemy import select, Row
@@ -31,6 +33,7 @@ def build_static_dataset(size: int) -> pd.DataFrame:
                 logging.info(f"Processing match with ID: {match.matchId}")
 
                 # Create a DataFrame for each match's data
+                match: SQLMatch
                 df_match = pd.DataFrame([match.get_training_data()])
                 logging.info("Match data fetched.")
 
@@ -41,43 +44,45 @@ def build_static_dataset(size: int) -> pd.DataFrame:
                 assert len(participants) == 10
 
                 for i, participant in enumerate(participants):
-                    logging.info(f"Processing participant {i} with puuid: {participant.puuid}")
-
+                    logging.info(f"Processing participant {i + 1} with puuid: {participant.puuid}")
+                    j = i + 1
                     # Fetch Summoner data
                     summoner = session.query(SQLSummoner).filter(SQLSummoner.puuid == participant.puuid).one()
                     df_summoner = pd.DataFrame([summoner.get_training_data()])
-                    df_summoner.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
+                    df_summoner.rename(columns=lambda x: f"participant{j}_" + x, inplace=True)
                     logging.info("Summoner data fetched.")
 
-                # Fetch championId from ParticipantStats
-                champion_id = session.query(SQLParticipantStats.championId).filter(
-                    SQLParticipantStats.participantId == participant.id).scalar()
-                logging.info(f"Fetched champion ID: {champion_id}")
+                    # Fetch championId from ParticipantStats
+                    champion_id = session.query(SQLParticipantStats.championId).filter(
+                        SQLParticipantStats.participantId == participant.id).scalar()
+                    logging.info(f"Fetched champion ID: {champion_id}")
 
-                # Fetch Summoner League data
-                summonerLeague = session.query(SQLSummonerLeague).filter(
-                    SQLSummonerLeague.puuid == participant.puuid).one()
-                df_summonerLeague = pd.DataFrame([summonerLeague.get_training_data()])
-                df_summonerLeague.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
-                logging.info("Summoner League data fetched.")
+                    # Fetch Summoner League data
+                    summonerLeague = session.query(SQLSummonerLeague).filter(
+                        SQLSummonerLeague.puuid == participant.puuid).one()
+                    df_summonerLeague = pd.DataFrame([summonerLeague.get_training_data()])
+                    df_summonerLeague.rename(columns=lambda x: f"participant{j}_" + x, inplace=True)
+                    logging.info("Summoner League data fetched.")
 
-                # Fetch Mastery data
-                mastery = session.query(SQLChampionMastery).filter(
-                    SQLChampionMastery.puuid == participant.puuid,
-                    SQLChampionMastery.championId == champion_id).scalar()
-                if mastery is None:
-                    df_mastery = pd.DataFrame([np.nan])
-                else:
-                    df_mastery = pd.DataFrame([mastery.get_training_data()])
-                    df_mastery.rename(columns=lambda x: f"participant{i}_" + x, inplace=True)
+                    # Fetch Mastery data
+                    mastery = session.query(SQLChampionMastery).filter(
+                        SQLChampionMastery.puuid == participant.puuid,
+                        SQLChampionMastery.championId == champion_id).scalar()
+                    if mastery is None:
+                        df_mastery = pd.DataFrame([np.nan])
+                    else:
+                        df_mastery = pd.DataFrame([mastery.get_training_data()])
+                        df_mastery.rename(columns=lambda x: f"participant{j}_" + x, inplace=True)
 
                     # Concatenate Summoner, Summoner League, and Mastery data to the match DataFrame
                     df_match = pd.concat([df_match, df_summoner, df_summonerLeague, df_mastery], axis=1, copy=False)
 
                 # Append this match's DataFrame to the overall DataFrame
                 data = pd.concat([data, df_match], axis=0, copy=False)
-                logging.info(f"Successfully processed match with ID: {match.matchId}")
             except Exception as e:
-                logging.error(f"An error occurred for match with ID {match.matchId}: {e}")
+                logging.error(f"An error occurred when processing match with ID {match.matchId}: {e}")
                 continue  # Skip match and continue with next match
+            logging.info(f"Successfully processed match with ID: {match.matchId}")
+    path = os.getcwd()
+    data.to_pickle("data/static_dataset.pkl")
     return data
